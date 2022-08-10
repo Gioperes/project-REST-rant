a6834ea701bfd6dc53010542e5a631a9aa257882
Gioperes <103082092+Gioperes@users.noreply.github.com>
2022-08-09 22:06:07 -0400

    Added mock data

--- rest-rant/controllers/places.js
+++ rest-rant/controllers/places.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
 
+//GET PLACES
 router.get('/', (req, res) => {
-    res.send('GET /places')
+    res.render('places/index')
 })
 
 module.exports = router
